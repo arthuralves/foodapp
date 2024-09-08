f66ff9f3fd24a40e3d71a9107e69a882be1b909d
Arthur Alves <arthur.alves.rs@gmail.comm>
2024-09-08 16:39:20 -0300

    feat: start categories managment feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
   devise_for :admins
   
   authenticated :admin do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_08_184851) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_08_193132) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_08_184851) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
