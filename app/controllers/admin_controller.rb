22949fa16c011a5645b0c7f94d9d3c69b29b9e4f
Arthur Alves <arthur.alves.rs@gmail.comm>
2024-09-08 16:02:54 -0300

    Implementing admin user with devise in the project

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,7 @@
+class AdminController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :admins
+  
+  authenticated :admin do
+    root to: "admin#index", as: :admin_root
+  end 
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "admin" => "admin#index"
   root "home#index"
 
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
