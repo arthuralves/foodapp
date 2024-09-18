f33b5c7b8ee880153aea185e72fe481ad445c7b3
Arthur Alves <arthur.alves.rs@gmail.comm>
2024-09-18 17:04:54 -0300

    implemented orders to products reference

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,7 +3,9 @@ class Admin::OrdersController < AdminController
 
   # GET /admin/orders or /admin/orders.json
   def index
-    @admin_orders = Order.all
+    @not_fulfilled_orders = Order.where(fullfiled: false).order(created_at: :asc)
+    @fulfilled_orders = Order.where(fullfiled: true).order(created_at: :asc)
+    # @admin_orders = Order.where(fullfiled: false).order(created_at: :asc)
   end
 
   # GET /admin/orders/1 or /admin/orders/1.json
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_18_193356) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_18_200114) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,6 +58,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_18_193356) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "order_id", null: false
+    t.string "size"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
     t.string "customer_email"
     t.boolean "fullfiled"
@@ -89,6 +100,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_18_193356) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "order_products", "orders"
+  add_foreign_key "order_products", "products"
   add_foreign_key "products", "categories"
   add_foreign_key "stocks", "products"
 end
