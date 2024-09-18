0c0cd22028987dd96057b83cbd4fbec1a0f2b43d
Arthur Alves <arthur.alves.rs@gmail.comm>
2024-09-18 12:38:11 -0300

    added stock feature on products

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :products
+    resources :products do
+      resources :stocks
+    end
+    
     resources :categories
   end
   devise_for :admins
-  
-  authenticated :admin do
+  root "home#index"
+
+  authenticated :admin_user do
     root to: "admin#index", as: :admin_root
   end 
 
   get "admin" => "admin#index"
-  root "home#index"
-
-
 end
