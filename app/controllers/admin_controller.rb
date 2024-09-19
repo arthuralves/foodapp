27b4ee48a9e85a9aed5cb13f3da79ffbe6110b84
Arthur Alves <arthur.alves.rs@gmail.comm>
2024-09-18 21:03:07 -0300

    including metrics on html

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,11 +3,11 @@ class AdminController < ApplicationController
     before_action :authenticate_admin!
     
     def index
-        @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
+        @orders = Order.where(fullfiled: false).order(created_at: :desc).take(5)
         @quick_stats = {
-            sales: Order.where(created: Time.now.midnight..Time.now).count,
-            revenue: Order.where(created: Time.now.midnight..Time.now).sum(:total).round(),
-            avg_sale: Order.where(created: Time.now.midnight..Time.now).average(:total).round(),
+            sales: Order.where(created_at: Time.now.midnight..Time.now).count,
+            revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).round(),
+            avg_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total).round(),
             per_sale: OrderProduct.joins(:order).where(
                 orders:{
                     created_at: Time.now.midnight..Time.now
