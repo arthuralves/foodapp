b6b8c137ef6c3bc2b07ea3739ad5c3dcf738a0cd
Arthur Alves <arthur.alves.rs@gmail.comm>
2024-09-27 18:12:32 -0300

    added production env keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  Rails.configuration.stripe = {
+    secret_key: ENV['STRIPE_SECRET_KEY'],
+    webhook_secret: ENV['WEBHOOK_SECRET_KEY']
+  }
 end
